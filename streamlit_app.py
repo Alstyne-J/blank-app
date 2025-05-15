47087bfd96e0b5f7bca42b21a1d49ff49fe2d64f
Alstyne-J <alstyne.js@gmail.com>
2025-05-15 14:07:51 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 st.title("🎈 My new app")
-st.image("17472913995366621957001635635397.jpg",width=200)
+st.image("17472913995366621957001635635397.jpg", width=200)
 st.wtite("\n")
 st.subheader("Asep Saepudin Ganteng")
 st.write("JANGAAAN LUPAAA STREEEAAAKKKK")
